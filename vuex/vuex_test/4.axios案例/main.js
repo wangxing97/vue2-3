de542d902f64e7e185cd6e8c599e2b84924d09e1
wangxing <1027923428@qq.com>
2026-01-06 14:08:11 +0800

    20260105 Done

--- vuex/vuex_test/4.axios案例/main.js
+++ vuex/vuex_test/4.axios案例/main.js
@@ -1,10 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import store from './store'
+
 Vue.config.productionTip = false
 
 window.vm =  new Vue({
-  store,
   render: h => h(App),
   beforeCreate(){
     Vue.prototype.$bus = this;
